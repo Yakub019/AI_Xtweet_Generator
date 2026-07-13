359fc358bb7f41fe45038c050c07ffd77131de1b
Your Name <you@example.com>
2026-07-14 00:34:26 +0530

    Remove unrelated folders, keep only LangGraph_Practice

--- RAG/DocLoad.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import TextLoader
-loader = TextLoader("poem.txt",autodetect_encoding=True)
-docs = loader.load()
-print(type(docs))
-print(len(docs))
-print(docs[0].metadata)
-print(type(docs[0]))
-
